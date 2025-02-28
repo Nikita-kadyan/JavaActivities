b9e32c3cc85f29d1cf2d07080b19a629b8fac349
Nikita-kadyan <nikkikadyan77@gmail.com>
2025-02-28 18:15:52 +0530

    Add files via upload

---
+++ CheckLeapYear.java
@@ -0,0 +1,23 @@
+class CheckLeapYear {
+    public static boolean isLeapYear(int year) {
+        if (year % 4 != 0) {
+            return false;
+        } else if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        int year = 2025;
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+}
